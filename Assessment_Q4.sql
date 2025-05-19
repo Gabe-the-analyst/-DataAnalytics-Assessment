f054f583231c35736d8018881e0f994bc2d6eb06
Gabriel Okonobo <163137186+Gabe-the-analyst@users.noreply.github.com>
2025-05-19 01:11:28 +0100

    Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,3 +1,5 @@
+-- Question 4: For each customer, assuming the profit_per_transaction is 0.1% of the transaction value, calculate: Account tenure (months since signup), Total transactions, Estimated CLV (Assume: CLV = (total_transactions / tenure) * 12 * avg_profit_per_transaction), and Order by estimated CLV from highest to lowest.
+
 WITH successful_txns AS (
     -- Step 1: Retrieve all successful savings transactions from accounts
     -- Filter by transaction status (case-insensitive check for success)
